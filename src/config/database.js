efcb48cc243d61ade206315d3ba9f36ce7d0f17c
Guilherme <guifcunha22@gmail.com>
2026-02-12 14:52:51 -0300

    feat: backend prepared for deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,35 @@
 import { Sequelize } from "sequelize";
 import path from "path";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv"; 
+
+dotenv.config();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: path.join(__dirname, "../database/database.sqlite"),
-    logging: false
-});
-export default sequelize
+
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        logging: false, 
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false 
+            }
+        }
+    });
+} else {
+    console.log("Rodando localmente com SQLite...");
+    sequelize = new Sequelize({
+        dialect: "sqlite",
+        storage: path.join(__dirname, "../database/database.sqlite"),
+        logging: false
+    });
+}
+
+export default sequelize;
